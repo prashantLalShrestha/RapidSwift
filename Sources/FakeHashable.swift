8f3d3be7089ca1274ff79bbc1652ed10f9628917
Prashant Shrestha <prashantlurvs@gmail.com>
2022-01-07 18:47:20 +0545

    FakeHashable implemented with dynamicMemberLookUp

--- Sources/FakeHashable.swift
+++ Sources/FakeHashable.swift
@@ -4,8 +4,9 @@
 
 import Foundation
 
+@dynamicMemberLookup
 public struct FakeHashable<T>: Hashable {
-    public let value: T
+    public private(set) var value: T
     
     public init(_ value: T) {
         self.value = value
@@ -13,6 +14,11 @@ public struct FakeHashable<T>: Hashable {
     
     public func hash(into hasher: inout Hasher) {
         
+    }
+    
+    public subscript<Value>(dynamicMember keyPath: WritableKeyPath<T, Value>) -> Value {
+        get { return value[keyPath: keyPath] }
+        set { value[keyPath: keyPath] = newValue }
     }
     
     public static func == (lhs: FakeHashable<T>, rhs: FakeHashable<T>) -> Bool {
